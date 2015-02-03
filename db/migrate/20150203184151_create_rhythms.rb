776a7ae6668cd9d06ca584bae98413291b6e3be5
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2015-02-03 10:59:51 -0800

    ran rhythm migration

--- db/migrate/20150203184151_create_rhythms.rb
+++ db/migrate/20150203184151_create_rhythms.rb
@@ -1,6 +1,9 @@
 class CreateRhythms < ActiveRecord::Migration
   def change
     create_table :rhythms do |t|
+      t.integer :creator_id, null: false
+      t.string :rhythm_str, null: false
+      t.integer :play_count, null: false
 
       t.timestamps null: false
     end
