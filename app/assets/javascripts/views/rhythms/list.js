291ee01726729d46d8a7af1e3921730730a3c499
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2015-02-05 19:31:29 -0800

    mini polygons are working

--- app/assets/javascripts/views/rhythms/list.js
+++ app/assets/javascripts/views/rhythms/list.js
@@ -16,24 +16,12 @@ Geometrhythm.Views.RhythmsList = Backbone.CompositeView.extend({
     this.listenTo(this.collection, 'remove', this.removeRhythmListItemView)
 
     this.users = options.users;
-
-    // if (options.creator) {
-      this.creatorId = options.creator;
-    //   this.filterByCreator();
-    // } else if (options.liker) {
-      this.likerId = options.liker;
-    //   this.filterByLiker();
-    // } else {
-    //
-    // }
+    this.creatorId = options.creator;
+    this.likerId = options.liker;
 
     this.collection.each(function(rhythm) {
       this.addRhythmListItemView(rhythm);
     }.bind(this))
-
-    // console.log("hey this might have stuff in it when we start");
-    // console.log(this.collection.filter);
-
   },
 
   render: function() {
@@ -67,77 +55,37 @@ Geometrhythm.Views.RhythmsList = Backbone.CompositeView.extend({
   },
 
   filterByCreator: function(event) {
-
-    // if (event) {
-    //   var creator_id = $(event.currentTarget).val();
-    // } else {
-    //   var creator_id = this.creatorId;
-    // }
-    //
-    // if (creator_id === "") {
-    //   this.creatorId = null;
-    //   this.collection.filter.creator_id = null;
-    //   this.collection.fetch();
-    // } else {
-    //   this.creatorId = creator_id;
-    //   this.collection.filter.creator_id = this.creatorId;
-    //   this.collection.fetchByFilter();
-    //   // debugger
-    // }
-
     this.creatorId = $(event.currentTarget).val();
     if (this.creatorId === "") {
       this.creatorId = null;
       delete this.collection.filter.creator_id;
-      //this.collection.fetch();
     } else {
       this.collection.filter.creator_id = this.creatorId;
-      // console.log(this.collection.filter);
-
     }
     this.collection.fetchByFilter();
-    // this.collection.fetch({ data: { creator_id: this.creatorId } });
   },
 
   filterByLiker: function(event) {
-
     this.likerId = $(event.currentTarget).val()
     if (this.likerId === "") {
       this.likerId = null;
       delete this.collection.filter.liker_id;
-      //this.collection.fetch();
     } else {
       this.collection.filter.liker_id = this.likerId;
-      // console.log(this.collection.filter);
-
     }
     this.collection.fetchByFilter();
   },
 
   filterByRhythmStr: function(event) {
-
     this.rhythmStr = $(event.currentTarget).val()
     if (this.rhythmStr === "") {
       this.rhythmStr = null;
       delete this.collection.filter.rhythm_str;
-      //this.collection.fetch();
     } else {
       this.collection.filter.rhythm_str = this.rhythmStr;
-      // console.log(this.collection.filter);
-
     }
-    // debugger
     this.collection.fetchByFilter();
   },
-
-  // filterByRhythmId: function(event) {
-  //
-  //   this.filterId = $(event.currentTarget).val()
-  //   this.collection.filter.id = this.filterId;
-  //   console.log(this.collection.filter);
-  //   this.collection.fetchByFilter();
-  //   // debugger
-  // },
 
   selectRhythm: function(event){
     event.preventDefault();
