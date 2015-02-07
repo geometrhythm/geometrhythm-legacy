95453e86c480fb6c1c318ffbcf71d3ac4df2e3e6
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2015-02-07 12:48:53 -0800

    f yeah i used a different technique to ensure the tempo stays constant

--- app/assets/javascripts/rhythm_ring/audioInterface.js
+++ app/assets/javascripts/rhythm_ring/audioInterface.js
@@ -14,24 +14,86 @@ $.RhythmRing.prototype.initializeAudio = function() {
 $.RhythmRing.prototype.changeTempo = function(event) {
   this.tempo = parseInt($(event.currentTarget).val());
   this.pulseDuration = ( 1000 / ( this.tempo / 60 ) ) / 4;
-  clearInterval(this.playingRhythm);
-  this.playRhythm();
+  // clearInterval(this.playingRhythm);
+  clearInterval(this.curPlaying);
+  // this.playRhythm();
+  this.playRhythm2();
 }
 
 $.RhythmRing.prototype.togglePlay = function() {
   if (this.paused) {
     this.paused = false;
-    this.playRhythm();
+    // this.playRhythm();
+    this.playRhythm2();
     $('#play-pause').addClass("active").html('<i style="font-size: 12px" class="glyphicon glyphicon-pause"></i>&nbsp;Pause');
   } else {
     this.paused = true;
-    clearInterval(this.playingRhythm);
+    // clearInterval(this.playingRhythm);
+    clearInterval(this.curPlaying);
     $('#play-pause').removeClass("active").html('<i style="font-size: 12px" class="glyphicon glyphicon-play"></i>&nbsp;Play');
   }
 }
 
-$.RhythmRing.prototype.playRhythm = function() {
-  this.playingRhythm = setInterval(function () {
+// $.RhythmRing.prototype.playRhythm = function() {
+//   this.playingRhythm = setInterval(function () {
+//
+//
+//     var fill = this.rhythmCells[this.playPos] ? '#333' : 'white';
+//     this.$el.find(".cell-handle[ord='" + this.playPos + "']:not('.grabbed')")
+//       .css('background-color', fill);
+//     this.$el.find(".cell[ord='" + this.playPos + "']")
+//       .css('background-color', fill);
+//
+//     this.playPos += this.playPos >= this.rhythmCells.length - 1 ? -(this.rhythmCells.length - 1) : 1
+//     if (this.playPos === this.rhythmCells.length - 1) {
+//       var that = this;
+//       var dbRhythm = Geometrhythm.Collections.rhythms.find( function(rhythm){
+//           return rhythm.get("rhythm_str") === that.rhythmAsStr();
+//         }
+//       );
+//       if (dbRhythm) {
+//         dbRhythm.set("play_count", dbRhythm.get("play_count") + 1);
+//         dbRhythm.save();
+//       }
+//     }
+//
+//     var fill = this.rhythmCells[this.playPos] ? 'orange' : 'cornsilk';
+//     if (this.playPos === 0 && this.rhythmCells[this.playPos]) fill = 'DodgerBlue';
+//     this.$el.find(".cell-handle[ord='" + this.playPos + "']:not('.grabbed')")
+//       .css('background-color', fill);
+//     this.$el.find(".cell[ord='" + this.playPos + "']")
+//       .css('background-color', fill);
+//
+//     if (this.rhythmCells[this.playPos]) {
+//       this.busses[this.curBus].volume = this.playPos === 0 ? 1 : 0.75
+//       this.busses[this.curBus].play();
+//       this.curBus += 1;
+//       if (this.curBus >= 4) {
+//         this.curBus = 0;
+//       }
+//     }
+//   }.bind(this), this.pulseDuration);
+// }
+//
+$.RhythmRing.prototype.manualOverrideRhythm = function(event) {
+  console.log("hey");
+  this.initializeRhythm($(event.currentTarget).val());
+}
+
+
+$.RhythmRing.prototype.playRhythm2 = function() {
+  this.start = new Date().getTime();
+  this.time = 0;
+  this.elapsed = '0.0';
+
+  this.playingRhythm2 = function() {
+    console.log("woot");
+    this.time += this.pulseDuration;
+    this.elapsed = Math.floor(this.time / 100) / 10;
+    if(Math.round(this.elapsed) == this.elapsed) { this.elapsed += '.0'; }
+    // document.title = this.elapsed;
+    var diff = (new Date().getTime() - this.start) - this.time;
+
     var fill = this.rhythmCells[this.playPos] ? '#333' : 'white';
     this.$el.find(".cell-handle[ord='" + this.playPos + "']:not('.grabbed')")
       .css('background-color', fill);
@@ -66,10 +128,9 @@ $.RhythmRing.prototype.playRhythm = function() {
         this.curBus = 0;
       }
     }
-  }.bind(this), this.pulseDuration);
-}
 
-$.RhythmRing.prototype.manualOverrideRhythm = function(event) {
-  console.log("hey");
-  this.initializeRhythm($(event.currentTarget).val());
+    this.curPlaying = setTimeout(this.playingRhythm2.bind(this), (this.pulseDuration - diff));
+  }.bind(this);
+
+  this.curPlaying = setTimeout(this.playingRhythm2.bind(this), (this.pulseDuration));
 }
